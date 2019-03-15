2827bd2d0c14031c716ec5ba282a11d6c570564c
Denis Gavrilyuk <karpa4o4@gmail.com>
2019-03-15 18:28:29 +0300

    Improve social auth api

--- users/views.py
+++ users/views.py
@@ -1,14 +1,27 @@
 """Module containing the class-based views related to creating and managing accounts in CusDeb. """
 
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.views.decorators.http import require_POST
+from django.views.decorators.cache import never_cache
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import status
+from social_core.actions import do_auth, do_complete, do_disconnect
+from social_core.utils import setting_name
+from social_django.views import _do_login
 
 from .serializers import CurrentUserSerializer, SocialTokenObtainPairSerializer
+from .utils import psa
+
+
+NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 
 
 class SignUpView(generics.CreateAPIView):
@@ -66,3 +79,30 @@ class GetTokenForSocial(View):
         if serializer.is_valid():
             return JsonResponse(serializer.validated_data, status=200)
         return JsonResponse(serializer.errors, status=400)
+
+
+@never_cache
+@psa(f'{NAMESPACE}:complete')
+def auth(request, _backend):
+    """Social authentication view. """
+    return do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
+
+
+@never_cache
+@csrf_exempt
+@psa(f'{NAMESPACE}:complete')
+def complete(request, _backend, *args, **kwargs):
+    """Authentication complete view. """
+    return do_complete(request.backend, _do_login, user=request.user,
+                       redirect_name=REDIRECT_FIELD_NAME, request=request, *args, **kwargs)
+
+
+@never_cache
+@login_required
+@psa()
+@require_POST
+@csrf_protect
+def disconnect(request, _backend, association_id=None):
+    """Disconnects given backend from current logged in user. """
+    return do_disconnect(request.backend, request.user, association_id,
+                         redirect_name=REDIRECT_FIELD_NAME)
