7c7ffd5abec000468067564382b46b540ea58bc9
Evgeny Golyshev <eugulixes@gmail.com>
2020-03-09 15:52:58 +0300

    Make username case-insensitive on signing in

--- cdapi/settings.py
+++ cdapi/settings.py
@@ -154,3 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+    'users.backend.CaseInsensitiveModelBackend',
+)
+
