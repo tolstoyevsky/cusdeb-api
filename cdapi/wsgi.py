ed2dd9bb2fe964073c925f1d982f0340541a0255
Evgeny Golyshev <eugulixes@gmail.com>
2020-03-29 23:24:03 +0300

    Set DJANGO_SETTINGS_MODULE to cdapi.docker

--- cdapi/wsgi.py
+++ cdapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdapi.docker')
 
 application = get_wsgi_application()  # pylint: disable=invalid-name
