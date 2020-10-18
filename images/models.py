d640df5ac2a33ea9c72583aa67cc99a41bccc355
Evgeny Golyshev <eugulixes@gmail.com>
2020-10-18 19:52:58 +0300

    Revert "Expand Image model with additional fields"
    This reverts commit 08f1e4abea5280c4b12d355fe86e62e99923fcac.

--- images/models.py
+++ images/models.py
@@ -1,7 +1,6 @@
 """Data models for the CusDeb API Images application. """
 
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class DistroName(models.Model):
@@ -80,6 +79,15 @@ class Device(models.Model):
         return '{} {}'.format(self.name, self.model)
 
 
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    device = models.ForeignKey(Device, models.PROTECT)
+    os = models.ForeignKey(OS, models.PROTECT)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
 class BuildTypeName(models.Model):
     """Build type name (i.e. Classic image, Mender-compatible image, Mender artifact). """
 
@@ -98,50 +106,6 @@ class BuildType(models.Model):
 
     class Meta:
         unique_together = (('device', 'os'), )
-
-    def __str__(self):
-        return '{} on {}'.format(self.os, self.device)
-
-
-class Image(models.Model):
-    """Model representing an image built by CusDeb. """
-
-    PENDING = 'pending'
-    BUILDING = 'building'
-    FAILED = 'failed'
-    SUCCEEDED = 'succeeded'
-
-    IMG_GZ = 'img.gz'
-    ART_MENDER = 'mender'
-
-    STATUS_CHOICES = (
-        (PENDING, 'Pending'),
-        (BUILDING, 'Building'),
-        (FAILED, 'Failed'),
-        (SUCCEEDED, 'Succeeded')
-    )
-    FORMAT_CHOICES = (
-        (IMG_GZ, 'Classic img.gz'),
-        (ART_MENDER, 'Mender artifact')
-    )
-
-    user = models.ForeignKey(User, models.PROTECT)
-    device = models.ForeignKey(Device, models.PROTECT)
-    os = models.ForeignKey(OS, models.PROTECT)
-    build_type = models.ForeignKey(BuildTypeName, models.PROTECT)
-    started_at = models.DateTimeField(auto_now_add=True, editable=False)
-    finished_at = models.DateTimeField(blank=True, null=True)
-    status = models.CharField(
-        max_length=18,
-        choices=STATUS_CHOICES,
-        default=PENDING,
-    )
-    format = models.CharField(
-        max_length=18,
-        choices=FORMAT_CHOICES,
-        default=IMG_GZ,
-    )
-    notes = models.TextField(default='')
 
     def __str__(self):
         return '{} on {}'.format(self.os, self.device)
