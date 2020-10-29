54c0df08f689d8d75e1ef01aaf26f6dbb45db45f
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-10-29 17:59:05 +0300

    Implement view for updating image notes

--- images/urls.py
+++ images/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import re_path
 
-from .views import ListImagesView
+from .views import ImageNotesUpdateView, ListImagesView
 
 
 urlpatterns = [  # pylint: disable=invalid-name
     re_path('all/?$', ListImagesView.as_view(), name='images-all'),
+    re_path('update_notes/$', ImageNotesUpdateView.as_view(), name='image-notes-update'),
 ]
