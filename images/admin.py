e4802b28bb7c1073a802f34512d34f7bb210c339
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-02-01 17:53:40 +0300

    Remove unused models from images application
    The following model are unused after after adoption pieman_devices:
    * DistroName
    * CodeName
    * Port
    * OS
    * DeviceName
    * Device
    * BuildTypeName
    * BuildType

--- images/admin.py
+++ images/admin.py
@@ -2,25 +2,7 @@
 
 from django.contrib import admin
 
-from .models import (BuildType, BuildTypeName, CodeName, Device, DeviceName,
-                     DistroName, Image, OS, Port)
+from .models import Image
 
 
-class HiddenModelAdmin(admin.ModelAdmin):
-    """Admin class for all the modules supposed to be hidden in the Django admin. """
-
-    def get_model_perms(self, request):
-        """Returns empty permissions dict to hide the target model in the Django admin. """
-
-        return {}
-
-
-admin.site.register(BuildType)
-admin.site.register(BuildTypeName, HiddenModelAdmin)
-admin.site.register(CodeName, HiddenModelAdmin)
-admin.site.register(Device)
-admin.site.register(DeviceName, HiddenModelAdmin)
-admin.site.register(DistroName, HiddenModelAdmin)
 admin.site.register(Image)
-admin.site.register(OS)
-admin.site.register(Port, HiddenModelAdmin)
--- images/serializers.py
+++ images/serializers.py
@@ -1,64 +1,8 @@
 """Module containing serializers for the CusDeb API Images application. """
 
-from django.db.models import Q
 from rest_framework import serializers
 
-from .models import Device, Image, OS, BuildType, BuildTypeName
-
-
-class OSSerializer(serializers.ModelSerializer):
-    """Serializes an OS. """
-
-    full_name = serializers.SerializerMethodField()
-    short_name = serializers.SerializerMethodField()
-    packages_url = serializers.StringRelatedField()
-    build_type = serializers.SerializerMethodField()
-
-    def get_build_type(self, obj):
-        """Returns either the specified build types (if any) or a build type by default. """
-
-        device_id = self.context.get('device_id')
-        build_types = BuildType.objects.filter(
-            Q(device=device_id) &
-            Q(os=obj.id)
-        ).values_list('build_type__name', flat=True)
-        if build_types:
-            return build_types
-
-        # If there are no build types associated with the OS, use the first one
-        # (i.e. pk=1) by default.
-        return [BuildTypeName.objects.get(pk=1).name]
-
-    def get_full_name(self, obj):  # pylint: disable=no-self-use
-        """Returns the full name of the operating system intended for a client. """
-
-        return str(OS.objects.get(pk=obj.id))
-
-    def get_short_name(self, obj):  # pylint: disable=no-self-use
-        """Returns the short name of the operating system intended for Pieman. """
-
-        return str(OS.objects.get(pk=obj.id).get_short_name())
-
-    class Meta:
-        model = OS
-        fields = ('id', 'full_name', 'short_name', 'build_type', 'packages_url', )
-
-
-class DeviceSerializer(serializers.ModelSerializer):
-    """Serializes a device. """
-
-    name = serializers.StringRelatedField()
-    os = serializers.SerializerMethodField()
-
-    def get_os(self, obj):  # pylint: disable=no-self-use
-        """Returns the list of OSes available for the device. """
-
-        queryset = obj.supported_os.all()
-        return OSSerializer(queryset, many=True, context={'device_id': obj.id}).data
-
-    class Meta:
-        model = Device
-        fields = ('id', 'name', 'generation', 'model', 'os', )
+from .models import Image
 
 
 class ImageSerializer(serializers.ModelSerializer):
