87f774171a3c6d16fed9aea798b8670838f3cce3
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-04-20 16:48:59 +0300

    Implement ImageDetailView

--- images/urls.py
+++ images/urls.py
@@ -2,11 +2,13 @@
 
 from django.urls import re_path
 
-from .views import ImageDeleteView, ImageNotesUpdateView, ListImagesView
+from .views import ImageDeleteView, ImageDetailView, ImageNotesUpdateView, ListImagesView
 
 
 urlpatterns = [  # pylint: disable=invalid-name
     re_path('all/?$', ListImagesView.as_view(), name='images-all'),
     re_path('delete/', ImageDeleteView.as_view(), name='image-delete'),
     re_path('update_notes/$', ImageNotesUpdateView.as_view(), name='image-notes-update'),
+    re_path('image_detail/(?P<image_id>[0-9a-f-]+)/$', ImageDetailView.as_view(),
+            name='image-detail'),
 ]
